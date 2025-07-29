e75be1f90226df033d71b98f1919d6c0328261d2
GuilhermeLondero <londero.guilherme@ufn.edu.br>
2025-07-29 00:28:47 +0000

    Exercícios aula

---
+++ 01.Revisão/programa5.c
@@ -0,0 +1,26 @@
+/*Sortear TAM números no vetor, porém não inserir números que
+já estejam no vetor, ao final, exibir os valores*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAM 10
+
+int main() {
+    int vetor[TAM];
+    srand(time(NULL));
+    int numero, posicaoUltimoNumero = 0, jaInserido;
+    for(int i = 0; i < TAM; i++){
+        numero = rand() % 100;
+        jaInserido = 0;
+        for (int j = 0; j <= posicaoUltimoNumero; j++){
+            if (numero == vetor[j]){
+                jaInserido = 1;
+                printf("Numero repetido gerado...")
+            }
+        }
+    }
+
+    return 0;
+}
